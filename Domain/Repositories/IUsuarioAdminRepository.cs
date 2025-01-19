fb4d83275c32ccac990e43ccc832ada995194929
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-01-19 09:42:31 -0300

    feat: repository do usuário admin

--- Domain/Repositories/IUsuarioAdminRepository.cs
+++ Domain/Repositories/IUsuarioAdminRepository.cs
@@ -4,6 +4,10 @@ namespace Domain.Repositories
 {
     public interface IUsuarioAdminRepository
     {
-        
+        void AddUsuarioAdmin(UsuarioAdmin usuarioAdmin);
+        void UpdateUsuarioAdmin(UsuarioAdmin usuarioAdmin);
+        void DeleteUsuarioAdmin(UsuarioAdmin usuarioAdmin);
+        Task<UsuarioAdmin?> GetUsuarioAdminByIdAsync(Guid id);
+        Task<UsuarioAdmin?> GetUsuarioAdminByEmailAsync(string email);
     }
 }
--- Persistence/Repositories/UsuarioAdminRepository.cs
+++ Persistence/Repositories/UsuarioAdminRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Domain.Entities.Users;
 using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Persistence.Context;
 
 namespace Persistence.Repositories
@@ -14,5 +15,20 @@ namespace Persistence.Repositories
         {
             
         }
+
+        public void AddUsuarioAdmin(UsuarioAdmin usuarioAdmin) => Add(usuarioAdmin);
+
+        public void UpdateUsuarioAdmin(UsuarioAdmin usuarioAdmin) => Update(usuarioAdmin);
+
+        public void DeleteUsuarioAdmin(UsuarioAdmin usuarioAdmin) => Delete(usuarioAdmin);
+
+        public Task<UsuarioAdmin?> GetUsuarioAdminByIdAsync(Guid id)
+            => FindByCondition(ua => ua.Id == id)
+                .FirstOrDefaultAsync();
+
+        public Task<UsuarioAdmin?> GetUsuarioAdminByEmailAsync(string email)
+               => FindByCondition(ua => ua.Email == email)
+                .FirstOrDefaultAsync();
+
     }
 }
