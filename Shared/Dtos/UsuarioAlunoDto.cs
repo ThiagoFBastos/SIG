542857e624c2a160d2b36c4518bd0c092dd87589
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-02-01 16:24:03 -0300

    fix: id da matrícula do usuário

--- Shared/Dtos/UsuarioAlunoDto.cs
+++ Shared/Dtos/UsuarioAlunoDto.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Domain.Entities.Users;
 using Shared.Dtos.Abstract;
 
 namespace Shared.Dtos
 {
     public record class UsuarioAlunoDto: UsuarioDto
     {
-        [JsonPropertyName("matricula_aluno")]
-        public required Guid Matricula { get; set; }
+        [JsonPropertyName("aluno_matricula")]
+        public required Guid AlunoMatricula { get; set; }
 
         [JsonPropertyName("aluno")]
         public required AlunoDto Aluno { get; set; }
+
+        public bool Match(UsuarioAluno usuario)
+        {
+            return usuario.Email == Email && usuario.Id == Id && usuario.AlunoMatricula == AlunoMatricula;
+        }
     }
 }
