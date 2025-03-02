eb90641fa750392e34784b6727c541332b203ecb
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-03-02 12:18:09 -0300

    feat: Criar uma classe AlunoTurmaDto sem nota

--- Shared/Dtos/AlunoTurmaDto.cs
+++ Shared/Dtos/AlunoTurmaDto.cs
@@ -7,31 +7,14 @@ using Domain.Entities;
 
 namespace Shared.Dtos
 {
-    public record class AlunoTurmaDto
+    public record class AlunoTurmaDto: AlunoTurmaSemNotaDto
     {
-        [JsonPropertyName("codigo")]
-        public required Guid Codigo { get; set; }
- 
-        [JsonPropertyName("matricula_aluno")]
-        public required Guid AlunoMatricula { get; set; }
-
-        [JsonPropertyName("aluno")]
-        public AlunoDto? Aluno { get; set;}
-
-        [JsonPropertyName("codigo_turma")]
-        public required Guid TurmaCodigo { get; set; }
-
-        [JsonPropertyName("turma")]
-        public TurmaDto? Turma { get; set; }
-
         [JsonPropertyName("nota")]
         public required double Nota { get; set; }
 
-        public bool Match(AlunoTurma alunoTurma)
+        public override bool Match(AlunoTurma alunoTurma)
         {
-            return AlunoMatricula == alunoTurma.AlunoMatricula &&
-            TurmaCodigo == alunoTurma.TurmaCodigo
-            && Nota == alunoTurma.Nota;
+            return base.Match(alunoTurma) && Nota == alunoTurma.Nota;
         }
     }
 }
