5754ea034d79df5ac2a517142dfc470fc956e24c
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-03-02 12:33:59 -0300

    chore: Criar TurmaDto com lista de alunos sem nota

---
+++ Shared/Dtos/TurmaSemNotaDto.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Shared.Dtos.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Shared.Dtos
+{
+    public record class TurmaSemNotaDto: TurmaSemAlunoTurmaDto
+    {
+        [JsonPropertyName("alunos")]
+        public List<AlunoTurmaSemNotaDto>? Alunos { get; set; }
+    }
+}
