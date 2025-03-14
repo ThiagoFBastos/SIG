cb693a8b703bd841f94949f67187021fe2092ec2
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-03-14 18:14:07 -0300

    docs: Documentar o controller UsuarioProfessorController com formatação específica

--- API/Controllers/UsuarioProfessorController.cs
+++ API/Controllers/UsuarioProfessorController.cs
@@ -11,18 +11,38 @@ using System.Security.Claims;
 
 namespace API.Controllers
 {
+    /// <summary>
+    /// Controlador responsável pelos usuários professores
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class UsuarioProfessorController : ControllerBase
     {
         private readonly IUsuarioProfessorService _usuarioProfessorService;
 
+        /// <summary>
+        /// Construtor do controlador UsuarioProfessorController
+        /// </summary>
+        /// <param name="usuarioProfessorService">Serviço responsável pelos casos de uso envolvendo os usuários professores</param>
         public UsuarioProfessorController(IUsuarioProfessorService usuarioProfessorService)
         {
             _usuarioProfessorService = usuarioProfessorService;
         }
 
+        /// <summary>
+        /// Gera um token de autenticação para o usuário professor
+        /// </summary>
+        /// <param name="loginDto">Parâmetros necessários para se autenticar um usuário como email e senha</param>
+        /// <response code="200">Token gerado com sucesso</response>
+        /// <response code="400">Parâmetros inválidos</response>
+        /// <response code="404">Usuário não encontrado</response>
+        /// <response code="401">Senha incorreta</response>
+        /// <returns>Um token JWT</returns>
         [HttpPost("login")]
+        [ProducesResponseType(typeof(JwtTokenDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> Login([FromBody] LoginUsuarioDto loginDto)
         {
             if (!ModelState.IsValid)
@@ -33,8 +53,23 @@ namespace API.Controllers
             return Ok(new JwtTokenDto { Token = jwtToken });
         }
 
+
+        /// <summary>
+        /// Altera a senha do usuário professor
+        /// </summary>
+        /// <param name="changeUsuarioPassword">Parâmetros necessários para se alterar a senha do usuário</param>
+        /// <response code="204">Senha alterada com sucesso</response>
+        /// <response code="400">Parâmetros inválidos</response>
+        /// <response code="404">Usuário não encontrado</response>
+        /// <response code="401">Senha incorreta</response>
+        /// <returns></returns>
+        /// <exception cref="BadRequestException">Exceção causada por passagem de dados inválidos</exception>
         [HttpPut("updatePassword")]
         [Authorize(Roles = "professor")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> UpdatePassword([FromBody] ChangeUsuarioPasswordDto changeUsuarioPassword)
         {
             if (!ModelState.IsValid)
@@ -55,9 +90,23 @@ namespace API.Controllers
             return NoContent();
         }
 
-
+        /// <summary>
+        /// Obtém-se os dados do usuário professor pelo identificador
+        /// </summary>
+        /// <param name="id">Identificador do usuário professor</param>
+        /// <param name="opcoes">Parâmetros adicionais para a requisição</param>
+        /// <response code="200">Dados do usuário professor retornados com sucesso</response>
+        /// <response code="400">Parâmetros inválidos</response>
+        /// <response code="401">Usuário não autorizado</response>
+        /// <response code="404">Usuário não encontrado</response>
+        /// <returns>Dados do usuário professor</returns>
+        /// <exception cref="BadRequestException">Exceção causada por passagem de dados inválidos</exception>
         [HttpGet("by/id/{id}")]
         [Authorize(Roles = "administrativo,admin,professor")]
+        [ProducesResponseType(typeof(UsuarioProfessorDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> GetById([FromRoute] Guid id, [FromQuery] GetUsuarioProfessorOptions? opcoes = null)
         {
             if(User.IsInRole("professor"))
@@ -80,8 +129,23 @@ namespace API.Controllers
             return Ok(usuarioDto);
         }
 
+        /// <summary>
+        /// Obtém-se os dados do usuário professor pelo email
+        /// </summary>
+        /// <param name="email">Email do usuário professor</param>
+        /// <param name="opcoes">Parâmetros adicionais para a requisição</param>
+        /// <response code="200">Dados do usuário professor retornados com sucesso</response>
+        /// <response code="400">Parâmetros inválidos</response>
+        /// <response code="401">Usuário não autorizado</response>
+        /// <response code="404">Usuário não encontrado</response>
+        /// <returns>Dados do usuário professor</returns>
+        /// <exception cref="BadRequestException">Exceção causada por passagem de dados inválidos</exception>
         [HttpGet("by/email/{email}")]
         [Authorize(Roles = "administrativo,admin,professor")]
+        [ProducesResponseType(typeof(UsuarioProfessorDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<IActionResult> GetByEmail([FromRoute] string email, [FromQuery] GetUsuarioProfessorOptions? opcoes)
         {
             if (User.IsInRole("professor"))
