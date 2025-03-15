86bbc24bb3f0dc61db5f2308b5b4250af80f3ad5
ThiagoFBastos <thiagobastos1705@gmail.com>
2025-03-15 17:13:07 -0300

    feat: Incluir JsonPropertyName na classe GetTurmasOptions

--- Shared.Pagination/GetTurmasOptions.cs
+++ Shared.Pagination/GetTurmasOptions.cs
@@ -11,21 +11,27 @@ namespace Shared.Pagination
     public record class GetTurmasOptions: GetOptions
     {
         [FromQuery(Name = "prefixo_disciplina")]
+        [JsonPropertyName("prefixo_disciplina")]
         public string? PrefixoDisciplina { get; init; }
 
         [FromQuery(Name = "ano_escolar")]
+        [JsonPropertyName("ano_escolar")]
         public int? AnoEscolar { get; init; }
 
         [FromQuery(Name = "a_partir_data")]
+        [JsonPropertyName("a_partir_data")]
         public DateTime ApartirData { get; init; } = DateTime.MinValue;
 
         [FromQuery(Name = "ate_data")]
+        [JsonPropertyName("ate_data")]
         public DateTime AteData { get; init; } = DateTime.MaxValue;
 
         [FromQuery(Name = "incluir_alunos")]
+        [JsonPropertyName("incluir_alunos")]
         public bool IncluirAlunos { get; init; } = false;
 
         [FromQuery(Name = "incluir_professor")]
+        [JsonPropertyName("incluir_professor")]
         public bool IncluirProfessor { get; init; } = false;
     }
 }
